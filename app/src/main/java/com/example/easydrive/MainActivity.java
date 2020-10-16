7a29a47944a0e2c39607ce45213d169cb3ff9452
Bar Zviely <zviely2003@gmail.com>
2020-10-16 12:55:22 +0300

    16.10

--- app/src/main/java/com/example/easydrive/MainActivity.java
+++ app/src/main/java/com/example/easydrive/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.easydrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void btnoption1(View view) {
+        Intent i = new Intent(this,)
+    }
+
+    public void btnoption2(View view) {
+    }
+}
 }
