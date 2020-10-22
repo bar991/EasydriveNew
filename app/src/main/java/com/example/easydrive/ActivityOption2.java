c32cc14ba6376ab3f37bb8d5d1f7234c67c586bf
Bar Zviely <zviely2003@gmail.com>
2020-10-22 21:31:08 +0300

    16.10

---
+++ app/src/main/java/com/example/easydrive/ActivityOption2.java
@@ -0,0 +1,14 @@
+package com.example.easydrive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityOption2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option2);
+    }
+}
--- app/src/main/java/com/example/easydrive/MainActivity.java
+++ app/src/main/java/com/example/easydrive/MainActivity.java
@@ -16,11 +16,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void btnoption1(View view) {
-        Intent i = new Intent(this,)
+
+
+
+    public void btnOption2(View view) { Intent i=new Intent(this,ActivityOption2.class);
+        startActivity(i);
     }
 
-    public void btnoption2(View view) {
+    public void btnOption3(View view) { Intent i=new Intent(this,ActivityOption3.class);
+        startActivity(i);
     }
-}
+
+    public void btnOption4(View view) { Intent i=new Intent(this,ActivityOption4.class);
+        startActivity(i);
+    }
+
+    public void btnOption7(View view) { Intent i=new Intent(this,ActivityOption7.class);
+        startActivity(i);
+    }
+
+    public void btnOption10(View view) { Intent i=new Intent(this,ActivityOption10.class);
+        startActivity(i);
+    }
+
+    public void btnOption9(View view) { Intent i=new Intent(this,ActivityOption9.class);
+        startActivity(i);
+    }
+
+    public void btnOption1(View view) { Intent i=new Intent(this,ActivityOption1.class);
+        startActivity(i);
+    }
+
+    public void btnOption8(View view) {
+        Intent i=new Intent(this,ActivityOption8.class);
+        startActivity(i);
+    }
+
+    public void btnOption5(View view) { Intent i=new Intent(this,ActivityOption5.class);
+        startActivity(i);
+    }
+
+    public void btnOption6(View view) { Intent i=new Intent(this,ActivityOption6.class);
+        startActivity(i);
+    }
 }
