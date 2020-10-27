b65d9b290068459959f8c927bcb124473c934cfd
Bar Zviely <zviely2003@gmail.com>
2020-10-27 22:28:47 +0200

    27.10.20

--- app/src/main/java/com/example/easydrive/ActivityOption10.java
+++ app/src/main/java/com/example/easydrive/ActivityOption10.java
@@ -2,7 +2,9 @@ package com.example.easydrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityOption10 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ActivityOption10 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option10);
+    }
+
+    public void gotologinpage(View view) {
+        Intent i=new Intent(this,ActivityOption1.class);
+        startActivity(i);
     }
 }
