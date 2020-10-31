af6de8ba529135302839651e2640954df875ce7b
Bar Zviely <zviely2003@gmail.com>
2020-10-31 18:30:52 +0200

    27.10.20

--- app/src/main/java/com/example/easydrive/ActivityOption1.java
+++ app/src/main/java/com/example/easydrive/ActivityOption1.java
@@ -2,7 +2,9 @@ package com.example.easydrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityOption1 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ActivityOption1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option1);
+    }
+    public void gotosignUp(View view) {
+        Intent i=new Intent(this,ActivityOption10.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/easydrive/ActivityOption2.java
+++ app/src/main/java/com/example/easydrive/ActivityOption2.java
@@ -2,7 +2,9 @@ package com.example.easydrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityOption2 extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ActivityOption2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option2);
+    }
+    public void gotoQA(View view) {
+        Intent i=new Intent(this,ActivityOption3.class);
+        startActivity(i);
+    }
+
+    public void gotoCM(View view) {
+        Intent i=new Intent(this,ActivityOption7.class);
+        startActivity(i);
     }
 }
